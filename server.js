f9b3d985be95a9f3493d3287be3fb5ebb0dbccc1
Palak Batra <palak99batra@gmail.com>
2024-12-29 14:41:54 +0530

    feat(UI): production changes 5

--- server.js
+++ server.js
@@ -26,15 +26,15 @@ console.log("hereeeeeeeeeeeeeeee");
 // API routes
 app.use('/api/v1/auth', authRoutes);
 
-// Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve(); // Get the absolute path of the project
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// // Serve static files in production
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve(); // Get the absolute path of the project
+//   app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // Define the port
 const PORT = process.env.PORT || 5000;
